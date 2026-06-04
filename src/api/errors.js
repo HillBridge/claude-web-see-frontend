b3228f5e1675ddfebba5c8e5bb378fde656cb75f
qiaoguosheng@pagsmile.com <294238921@qq.com>
2026-06-04 15:13:30 +0800

    删除接口url替换

--- src/api/errors.js
+++ src/api/errors.js
@@ -2,5 +2,5 @@ import request from '../utils/request';
 
 export const errorsApi = {
   // 删除错误分组及其全部关联数据(录屏 / 用户行为 / sourcemap)
-  removeGroup: (groupId) => request.delete(`/errorGroups/${groupId}`),
+  removeGroup: (groupId) => request.delete(`/api/errorGroups/${groupId}`),
 };
